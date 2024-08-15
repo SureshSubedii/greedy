f52be80d6c147107d8b5364ec345a7af87aef3dd
sureshsubedii <sureshsubedi485@gmail.com>
2024-08-15 17:05:05 +0545

    Optimize Lemonade change

--- lemonade_change.js
+++ lemonade_change.js
@@ -1,22 +1,21 @@
-
+//For bills 5,10,20
 var lemonadeChange = function(bills) {
-    let fives = []
-    let tens = []
-
+    let fives = 0
+    let tens = 0
     for(let i = 0; i < bills.length; i ++){
         let returnAmt = bills[i] - 5
-        if(bills[i] === 5) fives.push(5)
-        if(bills[i] === 10) tens.push(10)
+        if(bills[i] === 5) fives ++
+        if(bills[i] === 10) tens ++
           while(returnAmt >= 10 && returnAmt > 0 ){
-            let deduct = tens.pop()
-            if(!deduct) break
-            returnAmt  -= deduct
+            if(!tens) break
+            returnAmt  -= 10
+            tens -- 
 
         }
         while ( returnAmt > 0){
-            let deduct = fives.pop()
-            if(!deduct) break
-            returnAmt  -=  deduct
+            if(!fives) break
+            returnAmt  -=  5
+            fives --
         }
         if(returnAmt > 0) return false
       
