08f1f6b15c8042cce487d818e4ec6f2a5619c9e5
sureshsubedii <sureshsubedi485@gmail.com>
2024-09-14 15:57:17 +0545

    Solve gas station problem

---
+++ gas_station.js
@@ -0,0 +1,21 @@
+// There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+// Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, otherwise return -1. If there exists a solution, it is guaranteed to be unique
+
+var canCompleteCircuit = function(gas, cost) {
+    let totalTank = 0;
+    let currentTank = 0;
+    let startIndex = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        totalTank += gas[i] - cost[i];
+        currentTank += gas[i] - cost[i];
+        
+        if (currentTank < 0) {
+            startIndex = i + 1;
+            currentTank = 0;
+        }
+    }
+
+    return totalTank >= 0 ? startIndex : -1;
+};
