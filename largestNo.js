82f3cbc9c34d73cb5d3193b3c63e65cc8a9faffe
suresh_11 <sureshsubedi485@gmail.com>
2024-10-28 18:17:43 +0545

    find the largest num from array

---
+++ largestNo.js
@@ -0,0 +1,18 @@
+// Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+// Since the result may be very large, so you need to return a string instead of an integer.
+
+
+var largestNumber = function(nums) {
+      nums = nums.map(String);
+
+    nums.sort((a, b) => (b + a) - (a + b));
+
+    const result = nums.join('');
+
+    return result[0] === '0' ? '0' : result;
+    
+};
+console.log(largestNumber([20,99,151]))
+
+
